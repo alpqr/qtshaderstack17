132faba41968315ffb3d700df30df4ee293a738d
Laszlo Agocs <laszlo.agocs@qt.io>
2018-09-25 20:07:30 +0200

    Fix indentation

--- src/shadertools/qbakedshader.h
+++ src/shadertools/qbakedshader.h
@@ -71,7 +71,7 @@ public:
         Q_DECLARE_FLAGS(Flags, Flag)
 
         ShaderSourceVersion() { }
-    ShaderSourceVersion(int version_, Flags flags_ = Flags())
+        ShaderSourceVersion(int version_, Flags flags_ = Flags())
             : version(version_), flags(flags_)
         { }
 
